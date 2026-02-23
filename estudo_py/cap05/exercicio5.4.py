469c0a7b1dd0d221cc8e49bc9cbb681c3d1fe4b0
Jorge <jorgew965@gmail.com>
2026-02-23 20:14:55 -0300

    exercicio 5.4 feito
    completo no dia 23/02/2026

---
+++ estudo_py/cap05/exercicio5.4.py
@@ -0,0 +1,12 @@
+#fazer o programa anterior so quem somente com numeros impares
+# fim = int(input('Digite o fim')) 
+# x = 0
+# while x <= fim
+#   print(x)  
+#   x = x + 1 
+fim = int(input('Digite o fim'))
+x = 0 
+while x <= fim:
+    if x % 2 == 1:
+        print(x)
+    x = x   +1
