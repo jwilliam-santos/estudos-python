e3ce0641ecac707cfb45a5f80c4f3b7e60bd617a
Jorge <jorgew965@gmail.com>
2026-02-24 21:51:09 -0300

    exercicios 5.6 feito
    completo no dia 24/02/2026

--- estudo_py/cap05/exercicico5.5.py
+++ estudo_py/cap05/exercicico5.5.py
@@ -4,4 +4,5 @@ x = 3
 while x <= 30:
     print(x)
     x = x + 3
+    x = x+ 3
     
---
+++ estudo_py/cap05/exercicio5.6.py
@@ -0,0 +1,6 @@
+# altere um programa para exibir os resultados no mesmo formato de uma tabuda de multiplicação 
+n = int(input(' Tabuada de:'))
+x = 2
+while  x <= 20:
+    print (n + x)
+    x = x + 2
