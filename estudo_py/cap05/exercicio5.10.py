4b5f9f6d3b56e07a82038f4541850ddbacf1ab4a
Jorge <jorgew965@gmail.com>
2026-02-27 13:58:27 -0300

    exercicio 5.10 atualizado no dia 27/02/2026

--- estudo_py/cap05/exercicio5.10.py
+++ estudo_py/cap05/exercicio5.10.py
@@ -13,13 +13,13 @@ pontos = 0
 questao = 1
 while questao <= 3:
     respostas = input(f'Resposta questao {questao}')
-    if questao == 1 and (respostas == 'b' or respostas == 'B'):
+    if questao == 1 and (respostas == ' b' or respostas == ' B'):
         pontos = pontos + 1
-    if questao == 2 and (respostas== 'a'  or respostas == 'A'):
+    if questao == 2 and (respostas== ' a'  or respostas == ' A'):
         pontos = pontos + 1 
-    if questao == 3 and (respostas == 'd' or respostas == 'D'):
+    if questao == 3 and (respostas == ' d' or respostas == ' D'):
         pontos = pontos + 1
     questao += 1
-print(f'o alunos fez {pontos} pontos(s)')
+print(f'o alunos fez {pontos} pontos(s)' ) 
 
 
