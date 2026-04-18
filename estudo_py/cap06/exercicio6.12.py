c25ca6d63f29ecc0e25b1e6e077c11cc0b67db61
Jorge <jorgew965@gmail.com>
2026-04-18 13:51:00 -0300

    exercicio 6.12 feito
    completo no dia 18/04/2026

---
+++ estudo_py/cap06/exercicio6.12.py
@@ -0,0 +1,16 @@
+#Altere o Programa 6.11 de forma a imprimir o menor elemento da lista.
+#---------------------------------------------------------------------
+#Programa 6.11
+# L = [1,7,2,4]
+#máximo = L[0]
+#for e in L:
+#   if e > máximo:
+#       máximo  = e
+#print(máximo)
+#programa começa na proxima linha (11)
+L = [4,2,1,7]
+minimo = L[0]
+for e in L:
+   if e < minimo:
+       minimo  = e
+print(minimo)
