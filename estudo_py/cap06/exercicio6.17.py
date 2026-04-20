79666f54ffe0dbe7ef6ee0070d0bb3d2f55c3d9d
Jorge <jorgew965@gmail.com>
2026-04-20 16:18:16 -0300

    exercicio 6.17 feito
    completo no dia 20/04/2026

---
+++ estudo_py/cap06/exercicio6.17.py
@@ -0,0 +1,40 @@
+#O que acontece quando dois valores são iguais?
+#Rastreie o Programa 6.20, mas com a lista L = [3,3,1,5,4].
+#----------------------------------------------------------
+#Programa 6.20
+#L = [7, 4, 3, 12, 8]
+#fim = 5
+#while fim > 1:
+#    trocou = False
+#    x = 0
+#    while x < (fim - 1):
+#        if L[x] > L[x + 1]:
+#           trocou = True
+#            temp = L[x]
+#            L[x] = L[x + 1]
+#            L[x + 1] = temp
+#       x += 1
+#    if not trocou:
+#        break
+#    fim -= 1
+#for e in L:
+#    print(e)
+#Programa começa na proxíma linha (23)
+L = [3,3,1,5,4]
+fim = 5
+while fim > 1:
+    trocou = False
+    x = 0
+    while x < (fim - 1):
+        if L[x] > L[x + 1]:
+            trocou = True
+            temp = L[x]
+            L[x] = L[x + 1]
+            L[x + 1] = temp
+        x += 1
+    if not trocou:
+        break
+    fim -= 1
+for e in L:
+    print(e)
+# Os valores ficam um do lado do outro, no caso 1,3,3,4,5
