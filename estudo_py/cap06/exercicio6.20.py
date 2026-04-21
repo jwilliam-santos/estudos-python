e8a6e6183fd4c129c60aff3e231244f8a4af6e8b
Jorge <jorgew965@gmail.com>
2026-04-21 00:11:28 -0300

    exercicio 6.20 feito
    completo no dia 21/04/2026

---
+++ estudo_py/cap06/exercicio6.20.py
@@ -0,0 +1,10 @@
+#Escreva um programa que gere um dicionário, em que cada chave seja um caractere, e seu valor seja o número desse caractere encontrado em uma frase lida.
+#Exemplo: O rato -> { “O”:1, “r”:1, “a”:1, “t”:1, “o”:1}
+frase = input('Digite uma frase:')
+dicionario = {}
+for letra in frase:
+    if letra in dicionario:
+        dicionario[letra] += 1
+    else:
+        dicionario[letra] = 1
+print(dicionario)
