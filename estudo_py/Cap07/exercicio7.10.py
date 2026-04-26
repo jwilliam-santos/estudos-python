39e1c13e6771c82cab77fb35828a0bfd04f12f44
Jorge <jorgew965@gmail.com>
2026-04-26 11:08:41 -0300

    exercicio 7.10 atualizado dia 26/04/2026

--- estudo_py/Cap07/exercicio7.10.py
+++ estudo_py/Cap07/exercicio7.10.py
@@ -1,6 +1,54 @@
 #Modifique o Programa 7.2 de forma a utilizar uma lista de palavras.
 #No início, pergunte um número e calcule o índice da palavra a utilizar pela fórmula:
 #índice = (número * 776) % len(lista_de_palavras).
+#------------------------------------------------------------------------------------
+#palavra = input("Digite a palavra secreta:").lower().strip()
+#for x in range(100):
+#    print()
+#digitadas = []
+#acertos = []
+#erros = 0
+#while True:
+#    senha = ""
+#    for letra in palavra:
+#        senha += letra if letra in acertos else "."
+#    print(senha)
+#    if senha == palavra:
+#        print("Você acertou!")
+#        break
+#    tentativa = input("\nDigite uma letra:").lower().strip()
+#    if tentativa in digitadas:
+#
+#        print("Você já tentou esta letra!")
+#        continue
+#    else:
+#        digitadas += tentativa
+#        if tentativa in palavra:
+#            acertos += tentativa
+#        else:
+#            erros += 1
+#            print("Você errou!")
+#    print("X==:==\nX  :  ")
+#    print("X  O  " if erros >= 1 else "X")
+#    linha2 = ""
+#    if erros == 2:
+#        linha2 = "  |  "
+#    elif erros == 3:
+#        linha2 = " \|  "
+#    elif erros >= 4:
+#        linha2 = " \|/ "
+#    print(f"X{linha2}")
+#    linha3 = ""
+#    if erros == 5:
+#        linha3 += " /   "
+#   elif erros >= 6:
+#        linha3 += " / \ "
+#   print(f"X{linha3}")
+#    print("X\n===========")
+#    if erros == 6:
+#        print("Enforcado!")
+#        break
+#Programa começa na proxima linha (52)
 número = int(input('Digite um número'))
 lista_de_palavras = [
     "desenvolvimento", "python", "linux", "hardware",
