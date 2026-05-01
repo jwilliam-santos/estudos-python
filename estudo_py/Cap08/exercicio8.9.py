1f959c4845ab7f5f4c574140a8008948053a8453
Jorge <jorgew965@gmail.com>
2026-04-30 22:32:01 -0300

    exercicio 8.9 feito
    completo dia 30/04/2026

---
+++ estudo_py/Cap08/exercicio8.9.py
@@ -0,0 +1,3 @@
+#Rastreie o Programa 8.6 e compare o seu resultado com o apresentado
+#-------------------------------------------------------------------
+#R: o programa calcula o fatorial de 4 que gera o fatorial de 3 que gera o fatorial de 2 que gera o fatorial de 1 que é 1 
