7fa24318ed8456a965ccf083d01c12a76598e96f
Jorge William <jorgew965@gmail.com>
2026-06-10 19:38:16 -0300

    exercicio 9.21 feito
    completo dia 10/06/2026

---
+++ estudo_py/Cap09/exercicio9.21.py
@@ -0,0 +1,137 @@
+#Nas funções de altera e apaga, peça que o usuário confirme a alteração e exclusão do nome antes de realizar a operação em si.
+
+agenda = []
+
+
+def pede_nome():
+    return input("Nome: ")
+
+
+def pede_telefone():
+    return input("Telefone: ")
+
+
+def mostra_dados(nome, telefone):
+    print(f"Nome: {nome} Telefone: {telefone}")
+
+
+def pede_nome_arquivo():
+    return input("Nome do arquivo: ")
+
+
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower() == mnome:
+            return p
+    return None
+
+
+def novo():
+    nome = pede_nome()
+    telefone = pede_telefone()
+    agenda.append([nome, telefone])
+
+
+def apaga():
+    nome = pede_nome()
+    p = pesquisa(nome)
+    if p is not None:
+        confirmação = input("deseja apagar a agenda?, digite 's' se sim").strip().lower()
+        if confirmação == "s":
+            del agenda[p]
+        else:
+            print("nada foi apagado")
+        
+    else:
+        print("Nome não encontrado.")
+
+
+def altera():
+    p = pesquisa(pede_nome())
+    if p is not None:
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        print("Encontrado:")
+        mostra_dados(nome, telefone)
+        confirmação = input("deseja alterar?, digite 's' se sim").strip().lower()
+        if confirmação == "s":
+            nome = pede_nome()
+            telefone = pede_telefone()
+            agenda[p] = [nome, telefone]
+        else:
+            print("nada foi alterado")
+    else:
+        print("Nome não encontrado.")
+
+
+def lista():
+    print("\nAgenda\n\n------")
+    for e in agenda:
+        mostra_dados(e[0], e[1])
+    print("------\n")
+
+
+def lê():
+    global agenda
+    nome_arquivo = pede_nome_arquivo()
+    with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
+        agenda = []
+        for linha in arquivo.readlines():
+            nome, telefone = linha.strip().split("#")
+            agenda.append([nome, telefone])
+
+
+def grava():
+    nome_arquivo = pede_nome_arquivo()
+    with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
+        for e in agenda:
+            arquivo.write(f"{e[0]}#{e[1]}\n")
+
+
+def valida_faixa_inteiro(pergunta, início, fim):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if início <= valor <= fim:
+                return valor
+        except ValueError:
+            print(f"Valor inválido, favor digitar entre {início} e {fim}")
+
+
+def ordernar_lista():
+    agenda_ordenada = sorted(agenda)
+    print(f"A lista ordenada fica como {agenda_ordenada}")
+
+
+def menu():
+    print("""
+  1 – Novo
+  2 – Altera
+  3 – Apaga
+  4 – Lista
+  5 – Grava
+  6 – Lê
+  7 – Ordena a lista
+  0 – Sai
+""")
+    return valida_faixa_inteiro("Escolha uma opção: ", 0, 7)
+
+
+while opção := menu():
+    if opção == 0:
+        break
+    elif opção == 1:
+        novo()
+    elif opção == 2:
+        altera()
+    elif opção == 3:
+        apaga()
+    elif opção == 4:
+        lista()
+    elif opção == 5:
+        grava()
+    elif opção == 6:
+        lê()
+    elif opção == 7:
+        ordernar_lista()
