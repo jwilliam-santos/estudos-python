a4671b975748ddc09d5f5897ddad3b4b63dde4f2
Jorge William <jorgew965@gmail.com>
2026-06-09 21:17:20 -0300

    exercicio 9.18 feito
    completo dia 9/06/2026

---
+++ estudo_py/Cap09/exercicio9.18.py
@@ -0,0 +1,3 @@
+#O que acontece se nome ou telefone contiverem o caractere usado como separador em seus conteúdos?
+#Explique o problema e proponha uma solução.
+# se tiver o "#",tera um erro a ler o arquivo, pois o programa não reconhece o caractere "#", um possivel jeito e usando o \n
