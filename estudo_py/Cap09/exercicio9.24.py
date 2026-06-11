09c3d279f121c241fd07b4af323bd0cce8d2be8b
Jorge William <jorgew965@gmail.com>
2026-06-11 00:49:53 -0300

    Exercicio 9.24 feito

---
+++ estudo_py/Cap09/exercicio9.24.py
@@ -0,0 +1,3 @@
+#O que acontece com a agenda se ocorrer um erro de leitura ou gravação? 
+#Explique.
+#R:-> Se Houver erro de leitura a agenda não e carregada.Já se houver erro de gravação a agenda ñ será gravada.
