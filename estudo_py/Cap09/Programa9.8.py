20317ecabfd1d6b71b79996903cda437cd9f0bd5
Jorge William <jorgew965@gmail.com>
2026-06-11 23:01:25 -0300

    Programa 9.8

---
+++ estudo_py/Cap09/Programa9.8.py
@@ -0,0 +1,22 @@
+#filmes = {
+#    "drama": ["Cidadão Kane", "O Poderoso Chefão"],
+#    "comédia": ["Tempos Modernos", "American Pie", "Dr. Dolittle"],
+#    "policial": ["Chuva Negra", "Desejo de Matar", "Difícil de Matar"],
+#    "guerra": ["Rambo", "Platoon", "Tora!Tora!Tora!"],
+#}
+#with open("filmes.html", "w", encoding="utf-8") as página:
+#    página.write("""
+#<!DOCTYPE html>
+#<html lang="pt-BR">
+#<head>
+#<meta charset="utf-8">
+#<title>Filmes</title>
+#</head>
+#<body>
+#""")
+#    for c, v in filmes.items():
+#        página.write(f"<h1>{c}</h1>\n")
+#        for e in v:
+#            página.write(f"<h2>{e}</h2>\n")
+#    página.write("</body></html>")
+#Este Programa será Util nos exercicio 9.29 e 9.30
