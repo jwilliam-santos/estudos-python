bb8dbda3780592062ca8132864c3bc9df6914c4c
Jorge William <jorgew965@gmail.com>
2026-06-15 15:44:16 -0300

    exercicio 10.9 feito

---
+++ estudo_py/Cap10/exercicio10.9.py
@@ -0,0 +1,6 @@
+#Modifique o método resumo da classe Conta para exibir o nome e o telefone de cada cliente
+from clientes import Cliente
+from contas2 import Conta
+joão = Cliente("João da Silva","777-1234")
+maria = Cliente("Maria Silva","555-4321")
+conta = Conta([joão,maria],"999-111")
