768ecb48d8cadbafef0e50634f4d3a2d2b063b71
Jorge William <jorgew965@gmail.com>
2026-06-15 16:11:24 -0300

    classe banco

---
+++ estudo_py/Cap10/banco.py
@@ -0,0 +1,10 @@
+#Nome banco como pedido no livro
+class Banco:
+    def __init__(self,nome):
+        self.nome = nome
+        self.contas = []
+    def abre_conta(self,conta):
+        self.contas.append(conta)
+    def lista_contas(self):
+        for c in self.contas:
+            c.resumo()
