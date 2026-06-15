69affbe951a67e540a1e21d73019beb7db1ab6f4
Jorge William <jorgew965@gmail.com>
2026-06-15 15:34:57 -0300

    teste.py

---
+++ estudo_py/Cap10/teste.py
@@ -0,0 +1,6 @@
+#Nome "teste.py" como pedido no Livro
+from clientes import Cliente
+from contas import Conta
+joão = Cliente("João da Silva","777-1234")
+maria = Cliente("Maria Silva","555-4321")
+conta = Conta([joão,maria],"999-111")
