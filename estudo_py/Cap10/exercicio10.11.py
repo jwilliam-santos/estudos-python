eaeca60591374c3cf08696a77a5bbab52955b93e
Jorge William <jorgew965@gmail.com>
2026-06-17 01:01:13 -0300

    att 10.11

--- estudo_py/Cap10/exercicio10.11.py
+++ estudo_py/Cap10/exercicio10.11.py
@@ -3,21 +3,21 @@
 #Cada cidade tem nome e População.
 #Escreva um programa de testes que crie três estados com algumas cidades em cada um.
 #Exiba a População de cada estado como a soma da população de suas cidades.
-class Cidade: #COMPLETO
+class Cidade: 
     def __init__(self,nomecdd,populaçãocdd):
         self.nomecdd = nomecdd
         self.população = populaçãocdd
     
 class Estado:
-    def __init__(self,sigla="sp",cdd = [],):#COMPLETO
+    def __init__(self,sigla="sp",cdd = [],):
         
-        self.sigla = sigla #Pronto
+        self.sigla = sigla 
         self.cidades = cdd
     
-    def mostrar(self):#rr
+    def mostrar(self):
         
-        print(f" Sigla {self.sigla}, ")# MOSTRA SIGLA
-        for c in self.cidades:# MOSTRAR NOME CIDADE
+        print(f" Sigla {self.sigla}, ")
+        for c in self.cidades:
             print(f"Uma cidade é {c.nomecdd}, e sua Poupulação {c.população}\n")
 
 Sorocaba = Cidade("sorocaba",757)
